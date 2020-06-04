628c6f485199e94cf80e36be93292b97e3c7d0fe
jozefcambora <jozef.cambora@rohlik.cz>
2020-06-05 00:15:33 +0200

    more tests and commands

--- cypress/integration/integromat.spec.ts
+++ cypress/integration/integromat.spec.ts
@@ -1,16 +1,53 @@
 /// <reference types="cypress" />
 
+let data: any;
+
 describe(`Integromat assignment tests`, () => {
-  it(`TESTCASE 01`, () => {
-    cy.visit(Cypress.env(`loginUrl`))
-    cy.get(`[name="email"]`)
-      .type(Cypress.env(`userEmail`))
-    cy.get(`[name="password"]`)
-      .type(Cypress.env(`userPassword`))
-    cy.get(`[type="button"]`).click()
-    cy.get(`[href="/user"] > div`)
-      .contains(`Joztest Cambora`)
-      .click()
-    cy.get(`a[href="/logout"]`).click()
+  before(() => {
+    //set data from JSON file in fixtures
+    cy.fixture(`data.json`).then((d) => {
+      data = d;
+    });
   });
-})
+
+  context(`Mandatory tasks`, () => {
+    beforeEach(() => {
+      //calling custom command
+      cy.loginAndNameCheck(
+        data.loginUrl,
+        data.user.email,
+        data.user.password,
+        data.user.name
+      );
+    });
+    it(`TESTCASE 01`, () => {
+      //cy.get(`[href="/user"] > div:not(.nav-icon)`)
+      //  .as(`loggedUserName`)
+      //  .contains(data.user.name);
+      cy.get(`@loggedUserName`).click();
+      cy.get(`a[href="/logout"]`).click();
+    });
+
+    it.only(`TESTCASE 02`, () => {
+      cy.get(`div`).contains(`Data stores`).click();
+      cy.get(`button[data-name="Add data store"]`).click();
+      cy.get(`input[name="name"]`)
+        .click(`center`) //testcase said explicitly to click it
+        .clear()
+        .type(`Test Data Store`);
+      cy.get(`button[type="button"]`).contains(`Add`).click();
+      cy.get(`input[name="udt_name"]`)
+        .click() //testcase said explicitly to click it
+        .clear()
+        .type(`Test Data Structure`);
+      cy.get(`button[type="button"]`).contains(`Add item`).click();
+      cy.get(`select[name="type"]`).select(`Number`);
+      cy.get(`input[name="name"]`).last().type(`Number`);
+      cy.get(`span.custom-control-description`).contains(` Required`).click();
+      cy.get(`button[type="button"]`).contains(`Add`);
+      cy.get(`.i-panel-nested > .i-panel-footer > .btn-primary`).click();
+      cy.get(`a[aria-label="dismiss cookie message"]`).click();
+      cy.get(`button`).not(`[data-dismiss="modal"]`).contains(`Save`).click();
+    });
+  });
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,6 +11,18 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add(
+  `loginAndNameCheck`,
+  (loginPageUrl, email, password, name) => {
+    cy.visit(loginPageUrl);
+    cy.get(`[name="email"]`).type(email);
+    cy.get(`[name="password"]`).type(password);
+    cy.get(`[type="button"]`).click();
+    cy.get(`[href="/user"] > div:not(.nav-icon)`)
+      .as(`loggedUserName`)
+      .contains(name);
+  }
+);
 //
 //
 // -- This is a child command --
@@ -23,3 +35,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+declare namespace Cypress {
+  interface Chainable {
+    loginAndNameCheck(
+      loginPageUrl: string,
+      email: string,
+      password: string,
+      name: string
+    ): Chainable;
+  }
+}
