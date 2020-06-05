41e156d81aacb20acd44ae7e3fec3c5b4abe6442
jozefcambora <jozef.cambora@rohlik.cz>
2020-06-05 16:52:39 +0200

    added testcases 1,2,3

--- cypress/integration/integromat.spec.ts
+++ cypress/integration/integromat.spec.ts
@@ -9,45 +9,107 @@ describe(`Integromat assignment tests`, () => {
       data = d;
     });
   });
+  beforeEach(() => {
+    //calling custom command from support/commands.ts
+    cy.loginAndNameCheck(
+      data.baseUrl + data.loginPath,
+      data.user.email,
+      data.user.password,
+      data.user.name
+    );
+  });
+  afterEach(() => {
+    cy.logout();
+  });
 
   context(`Mandatory tasks`, () => {
-    beforeEach(() => {
-      //calling custom command
-      cy.loginAndNameCheck(
-        data.loginUrl,
-        data.user.email,
-        data.user.password,
-        data.user.name
-      );
-    });
     it(`TESTCASE 01`, () => {
-      //cy.get(`[href="/user"] > div:not(.nav-icon)`)
-      //  .as(`loggedUserName`)
-      //  .contains(data.user.name);
-      cy.get(`@loggedUserName`).click();
-      cy.get(`a[href="/logout"]`).click();
+      //nothing needs to be here
     });
 
-    it.only(`TESTCASE 02`, () => {
-      cy.get(`div`).contains(`Data stores`).click();
-      cy.get(`button[data-name="Add data store"]`).click();
-      cy.get(`input[name="name"]`)
-        .click(`center`) //testcase said explicitly to click it
-        .clear()
-        .type(`Test Data Store`);
-      cy.get(`button[type="button"]`).contains(`Add`).click();
-      cy.get(`input[name="udt_name"]`)
-        .click() //testcase said explicitly to click it
-        .clear()
-        .type(`Test Data Structure`);
-      cy.get(`button[type="button"]`).contains(`Add item`).click();
-      cy.get(`select[name="type"]`).select(`Number`);
-      cy.get(`input[name="name"]`).last().type(`Number`);
-      cy.get(`span.custom-control-description`).contains(` Required`).click();
-      cy.get(`button[type="button"]`).contains(`Add`);
-      cy.get(`.i-panel-nested > .i-panel-footer > .btn-primary`).click();
-      cy.get(`a[aria-label="dismiss cookie message"]`).click();
-      cy.get(`button`).not(`[data-dismiss="modal"]`).contains(`Save`).click();
+    it(`TESTCASE 02`, () => {
+      cy.gotoDataStores();
+      cy.createDataStore(`Test Data Store`, `Test Data Structure`);
+
+      //check that new data store exists ()
+      cy.checkDataStore(`Test Data Store`);
+      cy.gotoDataStructures();
+      cy.checkDataStructure(`Test Data Structure`);
+    });
+  });
+  context(`Clear data stores and structures`, () => {
+    it(`Delete data stores`, () => {
+      cy.gotoDataStores();
+
+      //look into the stores
+      cy.get(`#all.active.datastores`).then((stores) => {
+        //if there are visible stores
+        if (
+          stores.find(`.datastore:not([style="display: none;"])`).length > 0
+        ) {
+          //click on each delete button
+          cy.get(`button.i-remover`).each((el) => {
+            cy.wrap(el).click().click();
+          });
+        }
+      });
+      //go to data structures
+      cy.gotoDataStructures();
+      //look into data structures
+      cy.get(`.active.udts`).then((structures) => {
+        //if there are visible structures
+        if (structures.find(`.udt:not([style="display: none;"])`).length > 0) {
+          //click on each delete button
+          cy.get(`button.i-remover`).each((el) => {
+            cy.wrap(el).click().click();
+          });
+        }
+      });
+    });
+  });
+  context(`Optional tasks`, () => {
+    it(`TESTCASE 03`, () => {
+      cy.gotoDataStores();
+      cy.createDataStore(`Test Data Store`, `Test Data Structure`);
+      cy.checkDataStore(`Test Data Store`);
+      cy.gotoDataStructures();
+      cy.checkDataStructure(`Test Data Structure`);
+      //click on delete
+      cy.get(`button.i-remover`)
+        .click()
+        .should(`have.attr`, `data-rly`, `true`)
+        .and(`contain.text`, `Really?`)
+        .click();
+      //check popup
+      cy.get(`.modal-dialog`)
+        .as(`dialog`)
+        .find(`p`)
+        .should(
+          `have.text`,
+          `Can't delete UDT because it is used by those datastores: Test Data Store.`
+        );
+      //waiting for animation to end
+      cy.wait(300);
+      //close popup
+      cy.get(`@dialog`)
+        .find(`button[data-dismiss="modal"]`)
+        .contains(`Close`)
+        .click();
+      cy.gotoDataStores();
+      //take the ID from the button and visit the new page (cypress cannot handle newly opened windows)
+      cy.get(`button.i-browse-datastore`).then((btn) => {
+        const storeId = btn[0].attributes.getNamedItem(`data-id`).value;
+        cy.visit(`${data.baseUrl}/datastore/${storeId}/browse`);
+      });
+      //click Add button
+      cy.get(`button`).contains(`Add`).click();
+      //type 1 to key field
+      cy.get(`input[name="key"]`).type(`1`);
+      //type 2020 to number field
+      cy.get(`input[name="Number"]`).type(`2020`);
+      //save settings
+      cy.get(`button`).contains(`Save`).click();
+      cy.visit(data.baseUrl);
     });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,6 +23,109 @@ Cypress.Commands.add(
       .contains(name);
   }
 );
+
+Cypress.Commands.add(`logout`, () => {
+  cy.get(`@loggedUserName`).click();
+  cy.get(`a[href="/logout"]`).click();
+});
+
+Cypress.Commands.add(`gotoDataStores`, () => {
+  cy.get(`div`).contains(`Data stores`).click();
+});
+
+Cypress.Commands.add(`gotoDataStructures`, () => {
+  cy.get(`button.nav-more`).click();
+  cy.get(`.list-group-small`).contains(`Data structures`).click();
+});
+
+Cypress.Commands.add(`createDataStore`, (store, structure) => {
+  //click "Add data store" button
+  cy.get(`button[data-name="Add data store"]`).click();
+
+  //within "Add data store" popup
+  cy.get(`h4`)
+    .contains(`Add data store`)
+    .parents(`.modal-content`)
+    .as(`store`)
+    .within(() => {
+      cy.get(`input[name="name"]`)
+        .click() //testcase said explicitly to click it
+        .clear()
+        .type(store);
+      cy.get(`button`).contains(`Add`).click();
+    });
+
+  //within "Add data structure" popup
+  cy.contains(`Add data structure`)
+    .parents(`.i-panel`)
+    .as(`structure`)
+    .within(() => {
+      cy.get(`input[name="udt_name"]`)
+        .click() //testcase said explicitly to click it
+        .clear()
+        .type(structure);
+
+      cy.get(`button`).contains(`Add item`).click();
+    });
+
+  //within "Add data store" popup
+  cy.get(`h1`)
+    .contains(`Add item`)
+    .parents(`.i-panel-nested`)
+    .as(`item`)
+    .within(() => {
+      cy.get(`select[name="type"]`).select(`Number`);
+      cy.get(`input[name="name"]`).type(`Number`);
+      cy.get(`span.custom-control-description`).contains(` Required`).click();
+      cy.get(`button`).contains(`Add`).click();
+    });
+  //get the cookie message out of the way
+  cy.get(`a[aria-label="dismiss cookie message"]`).click();
+
+  //back in "Add data structure" popup
+  cy.get(`@structure`).within(() => {
+    cy.get(`button`).contains(`Save`).click();
+  });
+
+  //back to "Add data store" popup
+  cy.get(`@store`).within(() => {
+    //verify, that Test Data Structure exists in dropdown
+    cy.get(`select[name="udt"]`)
+      .as(`sel`)
+      .within((el) => {
+        cy.wrap(el).then((el) => {
+          if (el.find(`option`).length < 2) {
+            cy.log(`data structure combobox is short on structures`);
+            cy.wait(500);
+          } else {
+            return;
+          }
+        });
+      });
+    cy.get(`select[name="udt"]`)
+      .find(`option`)
+      .contains(structure)
+      .should(`have.text`, structure);
+    cy.get(`button`).contains(`Save`).click();
+  });
+});
+
+Cypress.Commands.add(`checkDataStore`, (store) => {
+  cy.get(`#all.active.datastores`)
+    .find(`.datastore:not([style="display: none;"])`)
+    .contains(store)
+    .should(`have.text`, store)
+    .and(`be.visible`);
+});
+
+Cypress.Commands.add(`checkDataStructure`, (structure) => {
+  cy.get(`.active.udts`)
+    .find(`.udt:not([style="display: none;"])`)
+    .contains(structure)
+    .should(`have.text`, structure)
+    .and(`be.visible`);
+});
+
 //
 //
 // -- This is a child command --
@@ -44,5 +147,12 @@ declare namespace Cypress {
       password: string,
       name: string
     ): Chainable;
+
+    createDataStore(store: string, structure: string): Chainable;
+    logout(): Chainable;
+    gotoDataStores(): Chainable;
+    gotoDataStructures(): Chainable;
+    checkDataStore(store: string): Chainable;
+    checkDataStructure(structure: string): Chainable;
   }
 }
